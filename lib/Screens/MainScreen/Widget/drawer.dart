6618f1d49a96b60c2e01d59d32e283c6168f230e
Ayush Mahawar <ayushmahawar60@gmail.com>
2021-10-04 13:52:42 +0530

    logout Solved

--- lib/Screens/MainScreen/Widget/drawer.dart
+++ lib/Screens/MainScreen/Widget/drawer.dart
@@ -7,132 +7,134 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class DrawerWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   Widget drawer(BuildContext context) {
-    final auth = FirebaseAuth.instance;
-    String? userName = auth.currentUser!.displayName;
+    String? userName = _auth.currentUser!.displayName;
     return SizedBox(
       width: MediaQuery.of(context).size.width.w * 0.85,
       child: Drawer(
-          child: Container(
-        color: Colors.white,
-        child: ListView(
-          children: [
-            SizedBox(height: 50.h),
-            DrawerHeader(
-              padding: EdgeInsets.symmetric(horizontal: 10.w),
-              child: UserAccountsDrawerHeader(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                ),
-                accountName: Text(
-                  userName.toString(),
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontWeight: FontWeight.w600,
-                    fontFamily: GoogleFonts.poppins().fontFamily,
-                    fontSize: 20.sp,
+        child: Container(
+          color: Colors.white,
+          child: ListView(
+            children: [
+              SizedBox(height: 50.h),
+              DrawerHeader(
+                padding: EdgeInsets.symmetric(horizontal: 10.w),
+                child: UserAccountsDrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
                   ),
+                  accountName: Text(
+                    userName.toString(),
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: GoogleFonts.poppins().fontFamily,
+                      fontSize: 20.sp,
+                    ),
+                  ),
+                  accountEmail: null,
+                  currentAccountPicture: Image(
+                      fit: BoxFit.cover,
+                      image: AssetImage("assets/apnikaksha.png")),
                 ),
-                accountEmail: null,
-                currentAccountPicture: Image(
-                    fit: BoxFit.cover,
-                    image: AssetImage("assets/apnikaksha.png")),
               ),
-            ),
-            listTileItem(
-                Icon(
-                  CupertinoIcons.home,
-                  size: 20.sp,
-                  color: Colors.grey,
-                ),
-                "Home",
-                null,
-                null, () {
-              print("Home");
-            }),
-            listTileItem(
-                Icon(
-                  CupertinoIcons.settings,
-                  color: Colors.grey,
-                  size: 18.sp,
-                ),
-                "Settings",
-                null,
-                null, () {
-              print("Setting");
-            }),
-            listTileItem(
-                Icon(
-                  Icons.format_align_left_rounded,
-                  size: 20.sp,
-                  color: Colors.grey,
-                ),
-                "Terms and Conditions",
-                null,
-                null, () {
-              print("TNC");
-            }),
-            listTileItem(
-                Icon(
-                  CupertinoIcons.signature,
-                  size: 20.sp,
-                  color: Colors.grey,
-                ),
-                "Privacy Policy",
-                null,
-                null, () {
-              print("Privacy Policy");
-            }),
-            listTileItem(
-                Icon(
-                  CupertinoIcons.repeat,
-                  size: 20.sp,
-                  color: Colors.grey,
-                ),
-                "Refund",
-                null,
-                null, () {
-              print("Refund");
-            }),
-            listTileItem(
-                Icon(
-                  Icons.star,
-                  color: Colors.yellow,
-                  size: 20.sp,
-                ),
-                "Rate",
-                true,
-                Colors.yellow, () {
-              print("Rate");
-            }),
-            listTileItem(
-                Icon(
-                  Icons.share,
-                  color: Colors.blue,
-                  size: 20.sp,
-                ),
-                "Share",
-                true,
-                Colors.blue, () {
-              print("Share");
-            }),
-            listTileItem(
-                Icon(
-                  Icons.logout,
-                  color: Colors.red,
-                  size: 20.sp,
-                ),
-                "Logout",
-                true,
-                Colors.red, () async {
-              await auth.signOut();
-              Navigator.of(context).pushAndRemoveUntil(
-                  MaterialPageRoute(builder: (context) => Wrapper()),
-                  (route) => false);
-            })
-          ],
+              listTileItem(
+                  Icon(
+                    CupertinoIcons.home,
+                    size: 20.sp,
+                  ),
+                  "Home",
+                  null,
+                  null, () {
+                print("Home");
+              }),
+              listTileItem(
+                  Icon(
+                    CupertinoIcons.settings,
+                    size: 20.sp,
+                  ),
+                  "Setting",
+                  null,
+                  null, () {
+                print("Setting");
+              }),
+              listTileItem(
+                  Icon(
+                    CupertinoIcons.signature,
+                    size: 20.sp,
+                  ),
+                  "Terms and Condition",
+                  null,
+                  null, () {
+                print("TNC");
+              }),
+              listTileItem(
+                  Icon(
+                    CupertinoIcons.pencil_ellipsis_rectangle,
+                    size: 20.sp,
+                  ),
+                  "Privacy Policy",
+                  null,
+                  null, () {
+                print("Privacy Policy");
+              }),
+              listTileItem(
+                  Icon(
+                    CupertinoIcons.return_icon,
+                    size: 20.sp,
+                  ),
+                  "Refund/Cancellation Policy",
+                  null,
+                  null, () {
+                print("Refund");
+              }),
+              listTileItem(
+                  Icon(
+                    CupertinoIcons.star_fill,
+                    size: 20.sp,
+                    color: Colors.yellow,
+                  ),
+                  "Rate",
+                  true,
+                  Colors.yellow, () {
+                print("Rate");
+              }),
+              listTileItem(
+                  Icon(
+                    Icons.share,
+                    size: 20.sp,
+                    color: Colors.blue,
+                  ),
+                  "Share",
+                  true,
+                  Colors.blue, () {
+                print("Share");
+              }),
+              listTileItem(
+                  Icon(
+                    Icons.logout,
+                    size: 20.sp,
+                    color: Colors.red,
+                  ),
+                  "Logout",
+                  true,
+                  Colors.red, () async {
+                try {
+                  _auth.signOut();
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => Wrapper()),
+                      (route) => false);
+                } catch (e) {
+                  print(e.toString() + " - > error");
+                  return;
+                }
+              })
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
@@ -143,18 +145,20 @@ Widget listTileItem(
 
   return Padding(
     padding: EdgeInsets.symmetric(vertical: 8.h),
-    child: ListTile(
+    child: InkWell(
       onTap: () {
         perform();
       },
-      leading: icon,
-      title: Text(
-        title,
-        style: TextStyle(
-          color: (custom == true) ? color : Colors.black,
-          fontSize: 16.sp,
-          fontFamily: poppins,
-          fontWeight: FontWeight.w600,
+      child: ListTile(
+        leading: icon,
+        title: Text(
+          title,
+          style: TextStyle(
+            color: (custom == true) ? color : Colors.black,
+            fontSize: 16.sp,
+            fontFamily: poppins,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
     ),
